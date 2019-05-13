596aa3fa8214f2552f288d6b333f5bed500d949d
Waseef Khan <kwaseef6030@gmail.com>
2019-05-13 17:33:55 +0500

    Added styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
+//= require popper
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-    validates :title, presence: true, length: {minimum: 3, maximum: 20}
-    validates :description, presence: true, length: {minimum: 10, maximum: 50}
+    validates :title, presence: true, length: {minimum: 5, maximum: 30}
+    validates :description, presence: true, length: {minimum: 10, maximum: 100}
 end
