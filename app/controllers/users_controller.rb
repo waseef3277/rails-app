6cf330a774b814b73f7482a5ea482c7d8149f488
Waseef Khan <kwaseef6030@gmail.com>
2019-05-15 18:34:41 +0500

    signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = "Welcome to the blog #{@user.username}"
 			redirect_to articles_path
 		else
